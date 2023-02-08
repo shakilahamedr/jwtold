6dfeae32873883e0ba9ba8390ff02ecaa32475f9
shakil <shakilahamed0607@gmail.com>
2023-02-08 11:22:34 +0530

    first commit

---
+++ jwtold/src/main/java/com/shakil/jwtold/Controller/Home.java
@@ -0,0 +1,15 @@
+package com.shakil.jwtold.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+	@RequestMapping("/welcome")
+	public String welcome() {
+		String text = "this is private page";
+		text+="this page is not allowed to unauthnticated users";
+		return text;
+	}
+}
---
+++ jwtold/src/main/java/com/shakil/jwtold/JwtoldApplication.java
@@ -0,0 +1,13 @@
+package com.shakil.jwtold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtoldApplication.class, args);
+	}
+
+}
